31fe15fe158a86bc4a2c30b971a67d9732d55161
Daniel Nilsson <danielnil@proton.me>
2026-08-29 11:50:55 +0200

    "warmup-1 question"

--- warmup-1/front_back.py
+++ warmup-1/front_back.py
@@ -1,3 +1,12 @@
+'''
+Given a string, return a new string where the first and last chars have been exchanged.
+
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+'''
+
 def front_back(str):
     if len(str) > 1: 
         first_char = str[0]
